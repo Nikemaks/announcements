bd31b8585c88e2a9ed7f7154aa0803cc99b5330c
Nikemaks <malyi.1992@mail.ru>
2020-09-02 15:19:22 +0300

    init project

--- applications/e2e/src/app.e2e-spec.ts
+++ applications/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('descAnnouncements app is running!');
+    expect(page.getTitleText()).toEqual('applications app is running!');
   });
 
   afterEach(async () => {
